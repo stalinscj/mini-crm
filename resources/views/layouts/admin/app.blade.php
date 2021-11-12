b4e744b8a8000404616c721397d1f20d0ade4e02
stalinscj <ssanchez684@gmail.com>
2021-11-11 21:47:04 -0400

    Install SweetAlert2

--- resources/views/layouts/admin/app.blade.php
+++ resources/views/layouts/admin/app.blade.php
@@ -70,6 +70,9 @@
   <!-- Bootstrap 4 -->
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-Piv4xVNRyMGpqkS2by6br4gNJ7DXjqk09RmUpJ8jgGtD7zP9yug3goQfGII0yAns" crossorigin="anonymous"></script>
 
+  <!-- Sweetalert -->
+  @include('sweetalert::alert', ['cdn' => "https://cdn.jsdelivr.net/npm/sweetalert2@9"])
+
   {{-- Plugins Scripts --}}
   @stack('plugins-js')
 
